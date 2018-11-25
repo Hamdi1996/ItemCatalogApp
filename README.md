ea7e1a7de74211da6177aee1a30a83e5aaccb7f9
unknown <hamedy_mhmoud@yahoo.com>
2018-11-25 19:54:05 +0200

    first commit

--- README.md
+++ README.md
@@ -1,39 +1,20 @@
-# Catalog Application
-This application is for Udacity's Full Stack Nanodegree catalog project.
+#Project Item-Catalog fullstackNanoDegree
+## Project Details
+-Install Vagrant and VirtualBox
+-Clone the fullstack-nanodegree-vm
+-Launch the Vagrant VM (vagrant up)
+-Write your Flask application locally in the vagrant/catalog directory (which will automatically be synced to /vagrant/catalog within the VM).
+-Run your application within the VM >>$'python second-pro-udacity.py'
+-Access and test your application by visiting http://localhost:5000 locally
 
-### Technologies used
-* [Python](https://www.python.org/) - Programming Language
-  * [Flask](http://flask.pocoo.org/) - Python framework
-  * [SQLAlchemy](https://www.sqlalchemy.org/) - Python SQL toolkit
-* [Vagrant](https://www.vagrantup.com/) - Virtual Machine
-* [Git](https://git-scm.com/) - Source code management
+### Requirements
+- python2.7
+- flask framework
+- oauth2client
+- functools
+- sqlalchemy
 
-### Getting up and running
-* Make sure you have Vagrant and Oracle VM Virtualbox installed.
-```
-# Spin up the VM
-vagrant up
-
-# Log into VM
-vagrant ssh
-
-# Enter project folder
-cd /vagrant
-
-# Setup databse file
-python database_setup.py
-
-# Create test items to put into the databse
-python createtestitems.py
-
-# Run the catalog application
-python project.py
-```
-* Once the project is running head to [localhost:5000](http://localhost:5000/)
-
-##### Note:
-This project is for the Udacity Full Stack Web Development Nanodegree program. The goal of this project was to create a catalog web
-application that provides a list of items within a variety of categories. This project integrated third party user registation using OAuth
-2.0 users that have accounts could then create, edit, and delete items and categories. This project also uses JSON endpoints to return JSON
-data to users that are logged into the site.
-# item
+####how to run 
+-in VM $'python second-pro-udacity.py'
+-visiting http://localhost:5000
+-done !
--- database_setup.py
+++ database_setup.py
@@ -1,4 +1,3 @@
-import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
@@ -7,62 +6,59 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 
-# Create class definitions for DB
 class User(Base):
-
     __tablename__ = 'user'
 
-    # Mappers for our users
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
     picture = Column(String(250))
 
 
-class Category(Base):
+class Restaurant(Base):
+    __tablename__ = 'restaurant'
 
-    __tablename__ = 'category'
-
-    # Mappers for our class category
     id = Column(Integer, primary_key=True)
-    name = Column(String(80), nullable=False)
-    items = relationship("CategoryItem", cascade="all, delete-orphan")
+    name = Column(String(250), nullable=False)
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
+    menu_item = relationship('MenuItem', cascade='all, delete-orphan')
 
     @property
     def serialize(self):
+        """Return object data in easily serializeable format"""
         return {
             'name': self.name,
             'id': self.id,
         }
 
 
-class CategoryItem(Base):
+class MenuItem(Base):
+    __tablename__ = 'menu_item'
 
-    __tablename__ = 'category_item'
-
-    # Mappers for our class category
     name = Column(String(80), nullable=False)
     id = Column(Integer, primary_key=True)
-    description = Column(String(1000))
-    os = Column(String(80))
-    category_id = Column(Integer, ForeignKey('category.id'))
-    category = relationship(Category)
+    description = Column(String(250))
+    price = Column(String(8))
+    course = Column(String(250))
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+    restaurant = relationship(Restaurant)
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
 
     @property
     def serialize(self):
-        # This returns object data in serialized format
+        """Return object data in easily serializeable format"""
         return {
             'name': self.name,
             'description': self.description,
             'id': self.id,
-            'os': self.os,
-            'user_id': self.user_id,
+            'price': self.price,
+            'course': self.course,
         }
 
-engine = create_engine('postgresql://catalog:catalog@localhost/catalog')
+
+engine = create_engine('postgresql://catalog:password@localhost/catalog')
+
 
 Base.metadata.create_all(engine)
